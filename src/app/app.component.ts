82848a0cb94cd53567ca2fc1caefde20bf69000b
nirajsakhare0033 <kangthecon0111@gmail.com>
2023-08-20 14:58:25 +0530

    todo task is done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'todo';
+
+  users = [];
+  users2 = [];
+
+  pushUser(event: any) {
+    this.users.push(event);
+  }
+
+  pushAdmin(event) {
+    this.users2.push(event);
+  }
+  removeitem(j) {
+    this.users.splice(j, 1);
+  }
+  removeitem2(j) {
+    this.users2.splice(j, 1);
+  }
 }
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
-  styleUrls: ['./child.component.css']
+  styleUrls: ['./child.component.css'],
 })
 export class ChildComponent {
+  @Input() boxcolor: any;
+  @Input() placeholder: any;
 
+  @Output() inputEvent = new EventEmitter();
+
+  count: number = 0;
+
+  createTask(box: any) {
+    if (box.value.length > 0) {
+      this.count = this.count + 1;
+      //alert(box.value);
+      this.inputEvent.emit(box.value);
+    }
+  }
 }
